8638cf19c40c31547880e582520e909b000cc2ad
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2024-01-28 12:43:06 +0900

    crypto zombieを動かす

---
+++ contracts/ZombieFactory.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.4.19;
+
+contract ZombieFactory {
+    // イベントをここで宣言するのだ
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // ここでイベントを発生させるのだ
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(bytes(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
