a00d6f94619e7033117166853c02707817f8132e
shibano <shibano@tmi.t.u-tokyo.ac.jp>
2024-02-01 17:10:03 +0900

    ethernal削除

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,4 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('hardhat-ethernal');
-
-// ETHERNAL_API_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJlYmFzZVVzZXJJZCI6InpqeUJzZ25qNnhQclFkM0lZUDlGbUVnUmhvazEiLCJhcGlLZXkiOiIzODg2OFdYLTY3Wk03MkgtSDJGWERCUy0xWVZTOFowXHUwMDAxIiwiaWF0IjoxNzA2NzQ0NDI4fQ.p-p8DgJLXsyJb-6CEzZlPiKd8biMNtNAd_O8FsEq61M
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
